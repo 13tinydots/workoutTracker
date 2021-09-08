18eae3f2f3cd4b592b9a50eefe26ea40a61b2b3b
Thomas Mccleary <thomasmccleary@Thomass-Mac-mini.local>
2021-09-08 13:40:54 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+
+ 
+import express from "express";
+import config from "./config.js";
+import apiRouter from "./routes.js";
+ 
+const app = express();
+ 
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+ 
+app.use("/api", apiRouter);
+ 
+app.listen(config.port, () => {
+  console.log(`Listening on port ${config.port}`);
+});
