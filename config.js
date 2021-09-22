daa3d5a7caf0fdb43cb77bbd9c55f26c9472c3ac
Thomas Mccleary <thomasmccleary@Thomass-Mac-mini.local>
2021-09-22 13:01:48 -0500

    workout working out

--- config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
- 
-dotenv.config();
- 
-export default {
-  port: process.env.PORT || 3000,
-  db: process.env.DB_CLIENT_URL,
-};
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-
- 
-import express from "express";
-import config from "./config.js";
-import apiRouter from "./routes.js";
- 
+const express = require("express");
+const mongoose = require("mongoose");
+const apiRouter = require("./routes")
+const PORT = process.env.PORT || 3001;
 const app = express();
  
-app.get("/", (_, res) => {
-  res.send("Hello World!");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+ 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
+
+app.use("/", apiRouter);
  
-app.use("/api", apiRouter);
- 
-app.listen(config.port, () => {
-  console.log(`Listening on port ${config.port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
